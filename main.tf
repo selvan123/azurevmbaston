138ec8dcb10cbce36b90abf5ad1738c026755810
selvan123 <selvans@gmail.com>
2025-08-08 16:45:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,10 +98,10 @@ resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
+ source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-focal" # Ubuntu Server 20.04 LTS
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
