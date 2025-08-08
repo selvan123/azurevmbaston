4292a5ca0a41a1d9c06d708ef21193f7438cc50e
selvan123 <selvans@gmail.com>
2025-08-08 16:09:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_DS1_v2"
-  admin_username      = "adminuser"
+  admin_username      = "devopsadmin"
   admin_password      = "P@ssword1234!"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
@@ -81,4 +81,27 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2025-datacenter-g2"
     version   = "latest"
   }
+
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
+  name                = "example-ubuntu-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "devopsadmin"
+  admin_password      = "P@ssword1234!"  # You can use SSH keys instead for better security
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
 }
